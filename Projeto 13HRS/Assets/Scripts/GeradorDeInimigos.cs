bcee45c8e2e934d48057d995451e162f619fc352
euduardo <53911823+happycodebarra@users.noreply.github.com>
2019-11-08 14:28:05 -0300

    spawn de inimigos 2.0

--- Projeto 13HRS/Assets/Scripts/GeradorDeInimigos.cs
+++ Projeto 13HRS/Assets/Scripts/GeradorDeInimigos.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GeradorDeInimigos : MonoBehaviour
 {
+    public Transform[] spawnpoint;
+
+    public GameObject inimigo;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       InvokeRepeating("randomizarSpawn", 0, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void randomizarSpawn()
+    {
+        int sorteio = Random.Range(0,spawnpoint.Length);
+
+        GameObject instance = Instantiate(inimigo, spawnpoint[sorteio].position, Quaternion.identity);
     }
 }
