10a4c0f204338d339e08ca046bd245373edeb5d8
euduardo <53911823+happycodebarra@users.noreply.github.com>
2019-11-29 14:18:28 -0300

    animacaio semi feita

---
+++ Projeto 13HRS/Assets/Scripts/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interface : MonoBehaviour
+{
+    public void iniciarGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
