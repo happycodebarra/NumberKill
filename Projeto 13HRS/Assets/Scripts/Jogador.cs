2031c5f784d392993339f27d819c374e45a6eea8
PC 8 <53911823+happycodebarra@users.noreply.github.com>
2019-12-06 13:41:25 -0300

    tiro

--- Projeto 13HRS/Assets/Scripts/Jogador.cs
+++ Projeto 13HRS/Assets/Scripts/Jogador.cs
@@ -22,11 +22,21 @@ public class Jogador : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            tiro();
+        }
+    }
+
     void tiro ()
     {
         RaycastHit hit;
-        if ( Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit ,100))
+        if ( Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit , 1000))
         {
+            Debug.Log(hit.transform.name);
+
             if (hit.transform.gameObject.tag == "Inimigo")
             {
                 Destroy(hit.transform.gameObject);
